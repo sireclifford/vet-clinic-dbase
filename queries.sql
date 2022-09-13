034a061c9d8d5d2afe75dbe58fc87cffe387d0fb
sireclifford <dev.sire.clifford@gmail.com>
2022-09-13 04:40:48 +0000

    write queries to retrieve info

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT * from animals WHERE neutered = '1' AND escape_attempts < 3;
+SELECT date_of_birth from animals where name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered = '1';
+SELECT * from animals WHERE name not like '%Gabumon%';
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
