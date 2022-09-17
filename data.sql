09fef3feb6fd41b736c33ab4e046608c0f0c714e
sireclifford <dev.sire.clifford@gmail.com>
2022-09-17 00:07:08 +0000

    write queries to query multiple tables

--- data.sql
+++ data.sql
@@ -11,3 +11,27 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
   
   INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
   VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
+
+  --insert into owners table
+  INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+  INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+  INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+  INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+  INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+  INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+
+  --insert into species table
+  INSERT INTO species (name) VALUES ('Pokemon');
+  INSERT INTO species (name) VALUES ('Digimon');
+
+  --update animals table with species_id
+  UPDATE animals SET species_id = 2 WHERE name LIKE = '%mon';
+  UPDATE animals SET species_id = 1 WHERE name NOT LIKE = '%mon';
+
+-- update animals table with owner_id
+  UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+  UPDATE animals SET owner_id = 2 WHERE name IN ('Pikachu', 'Gabumon');
+  UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+  UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+  UPDATE animals SET owner_id = 5 WHERE name IN ('Angeomon', 'Boarmon');
--- schema.sql
+++ schema.sql
@@ -8,3 +8,59 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg FLOAT
   );
+
+-- create table owners
+CREATE TABLE owners (
+	id SERIAL,
+  full_name VARCHAR(100),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+-- create table species
+CREATE TABLE species (
+  id SERIAL,
+  name VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
+--modify animals table
+BEGIN;
+DROP TABLE IF EXISTS animals;
+
+CREATE TABLE animals (
+  id SERIAL,
+  name VARCHAR(100),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg FLOAT,
+  species_id INT,
+  owner_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(species_id) REFERENCES species(id),
+  FOREIGN KEY(owner_id) REFERENCES owners(id)
+);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
+  VALUES (1, 'Agumon', '2020-02-03', 0, false, 10.23);
+  
+    INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
+  VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
+  
+      INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
+  VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
+  
+  INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
+  VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
+
+  SELECT * from animals WHERE name LIKE '%mon';
+SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT * from animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth from animals where name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered = true;
+SELECT * from animals WHERE name not like '%Gabumon%';
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+COMMIT;
