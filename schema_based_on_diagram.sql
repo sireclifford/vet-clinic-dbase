7a9206dd1cb477555868dbf767d2ca413acb4951
onomeh <onwuanibeonome@gmail.com>
2022-09-22 13:28:08 +0000

    Create table invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -32,3 +32,25 @@ CREATE TABLE invoices
     payed_at TIMESTAMP,
     medical_history_id INT
 );
+
+-- invoice_items table
+CREATE TABLE invoices_items
+(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT,
+    CONSTRAINT fk_invoices FOREIGN KEY (invoice_id) REFERENCES invoices (id) ON DELETE CASCADE,
+    CONSTRAINT fk_treatments FOREIGN KEY (treatment_id) REFERENCES treatments (id) ON DELETE CASCADE
+);
+-- table created from a many to many relationship between medical_histories and treatments
+CREATE TABLE medical_histories_treatments
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    medical_history_id INT,
+    treatment_id INT,
+    CONSTRAINT fk_medical_histories FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE CASCADE,
+    CONSTRAINT fk_treatments FOREIGN KEY (treatment_id) REFERENCES treatments (id) ON DELETE CASCADE
+)
