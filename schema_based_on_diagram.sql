146e936a95c10491d39cf5bfa8140970f5b565d3
aeh1707d <abdelhadi.hireche@outlook.com>
2022-09-22 14:24:34 +0100

    Tables treatement and invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,3 +15,20 @@ CREATE TABLE medical_histories
     status VARCHAR(20) NOT NULL,
     CONSTRAINT fk_patients FOREIGN KEY (patient_id) REFERENCES patients (id) ON DELETE CASCADE
 );
+
+-- treatments table
+CREATE TABLE treatments
+(
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    type VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL
+);
+-- invoices table
+CREATE TABLE invoices
+(
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP,
+    medical_history_id INT
+);
